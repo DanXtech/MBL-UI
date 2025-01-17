98f05253b419069eccde726c5c8f194ea5e188a0
Adetoye daniel kehinde <150145750+DanXtech@users.noreply.github.com>
2025-01-17 14:30:14 +0100

    Update Login.jsx

--- src/Pages/auth/Login.jsx
+++ src/Pages/auth/Login.jsx
@@ -1,121 +1,133 @@
-import { useEffect, useRef } from "react";
 import AuthImage from "../../assets/Red-and-Black-Monogram-Sports-Baseball-Club-Logo.png";
 import Button from "../../components/Button";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/authContext";
+import { useEffect } from "react";
 
-const Login = () => {
-  const { signInWithGoogle, user } = useAuth();
-  const navigate = useNavigate();
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
+const Signup = () => {
+    const { signInWithGoogle, user } = useAuth();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    if (user) {
-      navigate("/playsection");
-    }
-  }, [user, navigate]);
+    useEffect(() => {
+        if (user) {
+            navigate('/playsection');
+        }
+    }, [user, navigate]);
 
-  const handleGoogleSignIn = async () => {
-    try {
-      await signInWithGoogle();
-    } catch (error) {
-      console.error("Error signing in with Google:", error.message);
-    }
-  };
+    const handleGoogleSignIn = async () => {
+        try {
+            await signInWithGoogle();
+        } catch (error) {
+            console.error("Error signing in with Google:", error.message);
+        }
+    };
 
-  const handleLogin = (e: React.FormEvent) => {
-    e.preventDefault();
-    const email = emailRef.current?.value;
-    const password = passwordRef.current?.value;
+    return (
+        <div
+            style={{
+                backgroundImage: `url(${AuthImage})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+            }}
+            className="min-h-screen w-full bg-black relative"
+        >
+            {/* Overlay for deep black effect */}
+            <div className="absolute inset-0 bg-[#1B1B2E] bg-opacity-90"></div>
 
-    if (!email || !password) {
-      alert("Please fill in all fields.");
-      return;
-    }
+            {/* Content */}
+            <div className="relative z-10 min-h-screen flex flex-col justify-center items-center px-4 lg:px-10 text-white py-5 lg:py-10">
+                <h2 className="text-xl mb-4 text-center lg:text-3xl lg:mb-8 font-black uppercase">
+                    Start your journey with baseball gaming championship
+                </h2>
 
-    // Add your login logic here
-    console.log("Logging in with email:", email);
-  };
+                <div className="flex items-center w-full max-w-lg lg:max-w-2xl ">
+                    <div className="w-full py-8 px-6 bg-[#2C2C44] bg-opacity-70 rounded-lg border-4">
+                        {/* Form Header */}
+                        <div className="flex items-center gap-3 pb-6">
+                            <span className="bg-white h-10 w-10 rounded-full"></span>
+                            <h3 className="text-xl lg:text-2xl font-bold capitalize">
+                                Register to start playing with friends
+                            </h3>
+                        </div>
 
-  return (
-    <div
-      style={{
-        backgroundImage: `url(${AuthImage})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-      }}
-      className="h-screen w-full bg-black relative"
-    >
-      {/* Overlay */}
-      <div className="absolute inset-0 bg-[#1B1B2E] bg-opacity-90"></div>
+                        {/* Form Fields */}
+                        <div className="flex flex-col gap-4 max-h-[60vh] lg:max-h-full scrollbar-none overflow-y-auto">
+                            <div className="flex flex-col lg:flex-row gap-2">
+                                <input
+                                    type="text"
+                                    className="bg-transparent border p-3 flex-1 rounded-lg text-sm lg:text-base"
+                                    placeholder="First Name"
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    className="bg-transparent border p-3 flex-1 rounded-lg text-sm lg:text-base"
+                                    placeholder="Second Name"
+                                    required
+                                />
+                            </div>
+                            <div className="flex flex-col lg:flex-row gap-2">
+                                <input
+                                    type="email"
+                                    className="bg-transparent border p-3 flex-1 rounded-lg text-sm lg:text-base"
+                                    placeholder="Enter Your Email"
+                                    required
+                                />
+                                <input
+                                    type="number"
+                                    className="bg-transparent border p-3 flex-1 rounded-lg text-sm lg:text-base"
+                                    placeholder="Enter Your Number"
+                                    required
+                                />
+                            </div>
+                            <div className="flex flex-col lg:flex-row gap-2">
+                                <input
+                                    type="password"
+                                    className="bg-transparent border p-3 flex-1 rounded-lg text-sm lg:text-base"
+                                    placeholder="Enter Your Password"
+                                    required
+                                />
+                                <input
+                                    type="password"
+                                    className="bg-transparent border p-3 flex-1 rounded-lg text-sm lg:text-base"
+                                    placeholder="Confirm Your Password"
+                                    required
+                                />
+                            </div>
+                            <div className="flex flex-col lg:flex-row gap-2">
+                                <input
+                                    type="text"
+                                    className="bg-transparent border p-3 flex-1 rounded-lg text-sm lg:text-base"
+                                    placeholder="Enter your country name"
+                                    required
+                                />
+                            </div>
 
-      {/* Content */}
-      <div className="relative z-10 h-full flex flex-col justify-center items-center p-4 lg:p-10 text-white  py-5 lg:py-10">
-        <h2 className="text-xl mb-4 text-center lg:text-3xl lg:mb-8 font-black uppercase">
-          Continue Playing Baseball Gaming Championship
-        </h2>
+                            <p>
+                                Have an account?{" "}
+                                <Link to="/login">
+                                    <i>Login ?</i>
+                                </Link>
+                            </p>
 
-        <div className="flex items-center w-full max-w-lg lg:max-w-2xl">
-          <div className="w-full py-8 px-6 bg-[#2C2C44] bg-opacity-70 rounded-lg border-4">
-            {/* Header */}
-            <div className="flex items-center gap-3 pb-6">
-              <span className="bg-white h-10 w-10 rounded-full"></span>
-              <h3 className="text-xl lg:text-2xl font-bold capitalize">
-                Welcome Back to MBL
-              </h3>
+                            {/* Submit Button */}
+                            <Button className="border rounded-lg py-3 w-full text-sm lg:text-base">
+                                Sign Up
+                            </Button>
+
+                            <button
+                                onClick={handleGoogleSignIn}
+                                className="cursor-pointer bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-lg w-full"
+                            >
+                                Sign in with Google Account
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            {/* Form */}
-            <form
-              onSubmit={handleLogin}
-              className="flex flex-col gap-4 h-[350px] lg:h-full overflow-y-auto scrollbar-none"
-            >
-              <div className="flex flex-col lg:flex-row gap-2">
-                <input
-                  ref={emailRef}
-                  type="email"
-                  className="bg-transparent border p-3 flex-1 rounded-lg text-sm lg:text-base"
-                  placeholder="Enter Your Email"
-                  required
-                />
-                <input
-                  ref={passwordRef}
-                  type="password"
-                  className="bg-transparent border p-3 flex-1 rounded-lg text-sm lg:text-base"
-                  placeholder="Enter Your Password"
-                  required
-                />
-              </div>
-
-              <p>
-                Don't have an account?{" "}
-                <Link to="/register" className="underline hover:text-blue-400">
-                  <i>Register</i>
-                </Link>
-              </p>
-
-              <Button
-                type="submit"
-                className="border rounded-lg py-3 w-full text-sm lg:text-base"
-              >
-                Login
-              </Button>
-
-              <button
-                type="button"
-                onClick={handleGoogleSignIn}
-                className="bg-blue-500 hover:bg-blue-600 text-white py-3 px-6 rounded-lg"
-              >
-                Login with Google
-              </button>
-            </form>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
-export default Login;
+export default Signup;
